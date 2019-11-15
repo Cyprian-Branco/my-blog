38891cdf0ff640d66815bf1a4d1bb3f91c2d01ad
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-15 16:15:55 +0300

    add ability to post posts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,9 +11,9 @@ public class App {
     public static void main (String[] args) {
         staticFileLocation("/public");
 
-        get("/", (request, response) -> {
+        get("/posts/new", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "index.hbs");
+            return new ModelAndView(model, "newpost-form.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/posts/new", (request, response) -> {
@@ -27,8 +27,15 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             ArrayList<Post> posts = Post.getAll();
             model.put("posts", posts);
-
             return new ModelAndView(model, "index.hbs");
+
+        }, new HandlebarsTemplateEngine());
+        get("/posts/:id", (request, response) -> {
+          Map<String, Object> model = new HashMap<>();
+          int idOfPostToFind = Integer.parseInt(request.params(":id"));
+          Post foundPost = Post.findById(idOfPostToFind);
+          model.put("post", foundPost);
+          return new ModelAndView(model, "post-detail.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
