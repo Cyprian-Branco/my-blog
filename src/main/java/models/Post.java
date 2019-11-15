edb8e52f61e975dd056d2264be7ade486ef8e88c
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-15 12:51:28 +0300

    update all

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,33 @@
+package models;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Post {
+    private String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
+
+    public Post(String content){
+        this.content = content;
+        this.published =false;
+        this.createdAt = LocalDateTime.now();
+        instances.add(this);
+    }
+    public String getContent(){
+        return content;
+    }
+    public static ArrayList<Post> getAll(){
+        return instances;
+    }
+    public static void clearAllPosts(){
+        instances.clear();
+    }
+    public boolean getPublished(){
+        return this.published;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+}
